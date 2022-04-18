f8d902efa1373bab9938b3fdab2987836beb0c4a
Arpit <arpitkumar01923@gmail.com>
2022-04-18 12:22:52 +0530

    homepage css

--- src/pages/home/main/Main.jsx
+++ src/pages/home/main/Main.jsx
@@ -19,13 +19,15 @@ const Main = () => {
     <main className="container">
       {deals.map((sale,index) => (
         <>
-          <Link className="deals" to="/product">
+          <div className="deals">
             <img className="carousal" src={sale.deal} alt="deals.img" />
-          </Link>
+          </div>
+          <h1 className="home-heading">Sort By Categories</h1>
           <section className="sub-container">
             {sale.data.map((item) => (
                 <Link to={`/product/${item.title}`}  >
                   <div className="retina">
+                    <h1 className="banner">{item.title}</h1>
                     <img className="img-size" src={item.src} alt="card-image" />
                   </div>
                 </Link>
