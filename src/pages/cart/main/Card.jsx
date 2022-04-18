a12ef6b6df75586941eb154919f6b9b3bd39c2a0
Arpit <arpitkumar01923@gmail.com>
2022-04-18 17:04:56 +0530

    resolve css conflicts

--- src/pages/cart/main/Card.jsx
+++ src/pages/cart/main/Card.jsx
@@ -8,51 +8,57 @@ export const Card = () => {
     <main className="sub-container">
       {cartState.cart.map((product) => (
         <section className="padding-0 card horizontal" key={product._id}>
-          <img className="img" src={product.image} alt="cartItems" />
+          <div className="container1">
+            <section className="header">
+              <h2 className="text">
+                <strong>{product.brand}</strong>
+              </h2>
+              <h3 className="margin-bottom-0_5 brand">{product.title}</h3>
+              <p className="margin-bottom-0_5 price">{product.price}</p>
+              <section className="card-footer">
+                <button
+                  className="icon-button"
+                  onClick={() =>
+                    cartDispatch({
+                      type: "DECREASE_QUANTITY",
+                      payload: product,
+                    })
+                  }
+                >
+                  -
+                </button>
+                <div className="icon-quantity">{product.quantity}</div>
+                <button
+                  className="icon-button"
+                  onClick={() =>
+                    cartDispatch({
+                      type: "INCREASE_QUANTITY",
+                      payload: product,
+                    })
+                  }
+                >
+                  +
+                </button>
+              </section>
 
-          <section className="header">
-            <h1 className="text">
-              <strong>{product.brand}</strong>
-            </h1>
-            <h2 className="brand">{product.title}</h2>
-            <h3 className="price">{product.price}</h3>
-          </section>
-
-          <section className="card-footer">
-            <button
-              className="icon-button"
-              onClick={() =>
-                cartDispatch({ type: "DECREASE_QUANTITY", payload: product })
-              }
-            >
-              -
-            </button>
-            <div className="icon-quantity">{product.quantity}</div>
-            <button
-              className="icon-button"
-              onClick={() =>
-                cartDispatch({ type: "INCREASE_QUANTITY", payload: product })
-              }
-            >
-              +
-            </button>
-          </section>
-
-          <section
-            className="card-footer"
-            onClick={() =>
-              wishlistDispatch({
-                type: "ADD_TO_WISHLIST",
-                payload: product,
-              })
-            }
-          >
-            <div className="icon">
-              <a className="favourite" href="#">
-                <i className="fas fa-heart"></i> Add to wishlist
-              </a>
-            </div>
-          </section>
+              <section
+                className="card-footer"
+                onClick={() =>
+                  wishlistDispatch({
+                    type: "ADD_TO_WISHLIST",
+                    payload: product,
+                  })
+                }
+              >
+                <div className="margin-top-0_5 icon">
+                  <a className="favourite" href="#">
+                    <i className="fas fa-heart"></i> Add to wishlist
+                  </a>
+                </div>
+              </section>
+            </section>
+            <img className="img" src={product.image} alt="cartItems" />
+          </div>
         </section>
       ))}
     </main>
